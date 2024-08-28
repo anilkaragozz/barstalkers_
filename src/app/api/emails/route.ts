38526471546306e3601a30115cb745c9255c3b0c
anilkaragoz <anil.karagoz@d-teknoloji.com.tr>
2024-08-28 17:16:01 +0300

    fix

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -1,7 +1,7 @@
 import Email from "@/emails";
 import { Resend } from "resend";
 
-const resend = new Resend("re_JowNMUdJ_742PT9Hkr1K8E7EXGfifBCoD");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
   try {
